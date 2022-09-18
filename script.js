faeddfc3295b381b05d29511a9b7b6aaaf9a32a7
TheKleyn <104390108+TheKleyn@users.noreply.github.com>
2022-09-18 19:18:23 -0400

    complete basic

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
-let missile = document.getElementById('droplets')
+let missile = document.getElementById('droplets');
+
+let theMan = document.getElementById('nobanana');
 
 
 
-let missileMover = function (object) {
-    object.target.style.removeProperty('right');
-    object.target.style.left = '120px';
-    object.target.style.visibility = 'visible';
+const missileMover = function () {
+    /*object.target.style.removeProperty('right');
+    object.target.style.left = '120px';*/
+    missile.style.visibility = 'visible';
+    missile.style.right = '950px';
+    document.getElementById('nobanana').style.display = 'none';
+    document.getElementById('yesbanana').style.display = 'inline';
+
+
+    //document.getElementsByClassName('title')[0].innerHTML = 'fuck you';
 }
+
+theMan.onclick = missileMover;
+
+//theMan.addEventListener('onclick', )
+
 
