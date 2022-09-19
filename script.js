20db7b081ff4f17432107b8572420ed924e0d8be
TheKleyn <104390108+TheKleyn@users.noreply.github.com>
2022-09-19 16:39:14 -0400

    with thumbs and laugh

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let missile = document.getElementById('droplets');
 
 let theMan = document.getElementById('nobanana');
 
-
+let laugh = new Audio('./Media/laughter.mp3')
 
 const missileMover = function () {
     /*object.target.style.removeProperty('right');
@@ -15,11 +15,27 @@ const missileMover = function () {
     document.getElementById('yesbanana').style.display = 'inline';
     //document.getElementsByTagName('h2').style.visibility = 'visible';
     missile.className = "animateDroplets";
+    laugh.play();
+
 
     //document.getElementsByClassName('title')[0].innerHTML = 'fuck you';
 }
 
 theMan.onclick = missileMover;
+
+const thumbsup = function () {
+    document.getElementById('response').innerHTML = 'Thanks, let me grab you by the pussy...';
+    document.getElementById('response').style.visibility = 'visible';
+}
+const thumbsdown = function () {
+     document.getElementById('response').innerHTML = 'Suck my old cock...';
+     document.getElementById('response').style.visibility = 'visible';
+}
+
+document.getElementById('thumbsup').onclick = thumbsup;
+document.getElementById('thumbsdown').onclick = thumbsdown;
+
+
 
 //theMan.addEventListener('onclick', )
 
