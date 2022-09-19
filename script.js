4dc8a5088069b39b63c1fec4061d0041f24c4e43
TheKleyn <104390108+TheKleyn@users.noreply.github.com>
2022-09-19 10:29:47 -0400

    adjusted some more for iphone

--- script.js
+++ script.js
@@ -8,13 +8,13 @@ const missileMover = function () {
     /*object.target.style.removeProperty('right');
     object.target.style.left = '120px';*/
     missile.style.visibility = 'visible';
-    missile.style.left = '70px';
-    missile.style.transform = 'rotate(-70deg)';
-    missile.style.top = '390px';
+    //missile.style.left = '70px';
+    //missile.style.transform = 'rotate(-70deg)';
+    //missile.style.top = '390px';
     document.getElementById('nobanana').style.display = 'none';
     document.getElementById('yesbanana').style.display = 'inline';
     //document.getElementsByTagName('h2').style.visibility = 'visible';
-
+    missile.className = "animateDroplets";
 
     //document.getElementsByClassName('title')[0].innerHTML = 'fuck you';
 }
